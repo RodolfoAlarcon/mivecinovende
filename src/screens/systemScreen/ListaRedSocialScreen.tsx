7c89c87750e39ea722846fc9c147d714e68a000d
RodolfoAlarcon <86270747+RodolfoAlarcon@users.noreply.github.com>
2022-05-27 13:16:43 -0400

    listo
    listo

--- src/screens/systemScreen/ListaRedSocialScreen.tsx
+++ src/screens/systemScreen/ListaRedSocialScreen.tsx
@@ -13,33 +13,206 @@ export const ListaRedSocialScreen = (props: any) => {
 
 
     return (
-        <ScrollView>
-            <TouchableOpacity onPress={() => { }}  >
+        <View style={{flex:1}}>
+            <View 
+                style={{
+                    backgroundColor:'#dfa71b',
+                    width:50,
+                    height:50,
+                    justifyContent:'center',
+                    alignItems:'center',
+                    borderRadius:50,
+                    position:'absolute',
+                    right:10,
+                    bottom:10
+                }}
+            >
+                <Icon 
+                    size={35} color='white' name='plus'
+                />
+            </View>
+            <DataTable.Row style={{ height: 70, padding: 10 }}>
+                <DataTable.Cell style={{ flex: 0.3, }}>
+                    <View>
+                        <Image
+                            source={require('../../sources/img/facebook.png')}
+                            style={{ height: 40, width: 40, resizeMode:'contain' }}
+                        />
+                    </View>
 
-                <DataTable.Row style={{ height: 70, padding: 10 }}>
-                    <DataTable.Cell style={{ flex: 0.3, backgroundColor: 'red' }}>
-                        <View>
-                            <Icon size={30} color="grey" name={"log-out"} />
-                        </View>
+                </DataTable.Cell>
+                <DataTable.Cell style={{ flex: 2, }}>
+                    <Text style={{ fontSize: 20 }} >
+                        Facebook
+                    </Text>
+                </DataTable.Cell>
 
-                    </DataTable.Cell>
-                    <DataTable.Cell style={{ flex: 2, backgroundColor: 'yellow' }}>
-                        <Text style={{ fontSize: 20 }} > Cerrar Sesión </Text>
-                    </DataTable.Cell>
+                <DataTable.Cell style={{ flex: 0.7, flexDirection: 'row' }}>
 
-                    <DataTable.Cell style={{ flex: 0.7, backgroundColor: 'blue', flexDirection:'row'}}>
+                    <TouchableOpacity
+                        style={{
+                            paddingHorizontal: 5
+                        }}
+                    >
+                        <Icon size={30} color='gray' name='edit' />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={{
+                            paddingHorizontal: 5
+                        }}
+                    >
+                        <Icon size={30} color='gray' name='trash-2' />
+                    </TouchableOpacity>
 
-                        <View style={{ justifyContent: 'center', flex:1,width:'50%' }}>
-                            <Icon size={30} color='gray' name='edit' />
-                        </View>
-                        <View style={{ justifyContent: 'center', flex:1, width:'50%'}}>
-                            <Icon size={30} color='gray' name='trash-2' />
-                        </View>
-                    </DataTable.Cell>
+                </DataTable.Cell>
 
-                </DataTable.Row>
-            </TouchableOpacity>
-        </ScrollView>
+            </DataTable.Row>
+            <DataTable.Row style={{ height: 70, padding: 10 }}>
+                <DataTable.Cell style={{ flex: 0.3, }}>
+                    <View>
+                        <Image
+                            source={require('../../sources/img/facebook.png')}
+                            style={{ height: 40, width: 40, resizeMode:'contain' }}
+                        />
+                    </View>
+
+                </DataTable.Cell>
+                <DataTable.Cell style={{ flex: 2, }}>
+                    <Text style={{ fontSize: 20 }} >
+                        Instagram
+                    </Text>
+                </DataTable.Cell>
+
+                <DataTable.Cell style={{ flex: 0.7, flexDirection: 'row' }}>
+
+                    <TouchableOpacity
+                        style={{
+                            paddingHorizontal: 5
+                        }}
+                    >
+                        <Icon size={30} color='gray' name='edit' />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={{
+                            paddingHorizontal: 5
+                        }}
+                    >
+                        <Icon size={30} color='gray' name='trash-2' />
+                    </TouchableOpacity>
+
+                </DataTable.Cell>
+
+            </DataTable.Row>
+            <DataTable.Row style={{ height: 70, padding: 10 }}>
+                <DataTable.Cell style={{ flex: 0.3, }}>
+                    <View>
+                        <Image
+                            source={require('../../sources/img/facebook.png')}
+                            style={{ height: 40, width: 40, resizeMode:'contain' }}
+                        />
+                    </View>
+
+                </DataTable.Cell>
+                <DataTable.Cell style={{ flex: 2, }}>
+                    <Text style={{ fontSize: 20 }} >
+                        Tik Tok
+                    </Text>
+                </DataTable.Cell>
+
+                <DataTable.Cell style={{ flex: 0.7, flexDirection: 'row' }}>
+
+                    <TouchableOpacity
+                        style={{
+                            paddingHorizontal: 5
+                        }}
+                    >
+                        <Icon size={30} color='gray' name='edit' />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={{
+                            paddingHorizontal: 5
+                        }}
+                    >
+                        <Icon size={30} color='gray' name='trash-2' />
+                    </TouchableOpacity>
+
+                </DataTable.Cell>
+
+            </DataTable.Row>
+            <DataTable.Row style={{ height: 70, padding: 10 }}>
+                <DataTable.Cell style={{ flex: 0.3, }}>
+                    <View>
+                        <Image
+                            source={require('../../sources/img/facebook.png')}
+                            style={{ height: 40, width: 40, resizeMode:'contain' }}
+                        />
+                    </View>
+
+                </DataTable.Cell>
+                <DataTable.Cell style={{ flex: 2, }}>
+                    <Text style={{ fontSize: 20 }} >
+                        Youtube
+                    </Text>
+                </DataTable.Cell>
+
+                <DataTable.Cell style={{ flex: 0.7, flexDirection: 'row' }}>
+
+                    <TouchableOpacity
+                        style={{
+                            paddingHorizontal: 5
+                        }}
+                    >
+                        <Icon size={30} color='gray' name='edit' />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={{
+                            paddingHorizontal: 5
+                        }}
+                    >
+                        <Icon size={30} color='gray' name='trash-2' />
+                    </TouchableOpacity>
+
+                </DataTable.Cell>
+
+            </DataTable.Row>
+            <DataTable.Row style={{ height: 70, padding: 10 }}>
+                <DataTable.Cell style={{ flex: 0.3, }}>
+                    <View>
+                        <Image
+                            source={require('../../sources/img/facebook.png')}
+                            style={{ height: 40, width: 40, resizeMode:'contain' }}
+                        />
+                    </View>
+
+                </DataTable.Cell>
+                <DataTable.Cell style={{ flex: 2, }}>
+                    <Text style={{ fontSize: 20 }} >
+                        Web
+                    </Text>
+                </DataTable.Cell>
+
+                <DataTable.Cell style={{ flex: 0.7, flexDirection: 'row' }}>
+
+                    <TouchableOpacity
+                        style={{
+                            paddingHorizontal: 5
+                        }}
+                    >
+                        <Icon size={30} color='gray' name='edit' />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        style={{
+                            paddingHorizontal: 5
+                        }}
+                    >
+                        <Icon size={30} color='gray' name='trash-2' />
+                    </TouchableOpacity>
+
+                </DataTable.Cell>
+
+            </DataTable.Row>
+
+        </View>
     )
     function goToScreen(routeName: any) {
         navigator.navigate(routeName as never);
